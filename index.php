043724c51cdad8ad3ed145dd4814f1a7f88fa745
Fernando Almeida <f_silvadealmeida@hotmail.com>
2021-08-02 16:04:01 -0300

    Adicionando o novo home.php no index.php

--- index.php
+++ index.php
@@ -18,14 +18,14 @@
             </nav>
         </header>
         <img src="imagens/fundo-topo2.jpg" width="1024px" height="400" alt="Modelo femino comendo doce">
-        
+
         <article class="portfolio">
             <h1>New Fashion Everything Never Enough</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris volutpat nisi non ante dictum congue. Cras id, posuere velit sed, lacinia mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed vehicula, ipsum vel congue cursus, mi neque tincidunt enim, egestas porttitor felis neque et risus. Maecenas fringilla hendrerit enim malesuada laoreet. Aenean quis tortor a odio tempus tincidunt. Vivamus rutrum nulla sed metus congue bibendum. Sed ut aliquam purus. Donec odio mauris, commodo sed dignissim vitae, dapibus eget mauris.</p>
         </article>
 
 <?php
-    include 'paginas/home-2.php';
+    include 'paginas/home.php';
 ?>
             
     <div id="contact">
