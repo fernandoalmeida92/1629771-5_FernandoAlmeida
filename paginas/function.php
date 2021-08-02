641f5f5630647bf92364abf47dc3e41c6b313159
Fernando Almeida <f_silvadealmeida@hotmail.com>
2021-08-02 15:58:27 -0300

    Adicionando uma função nova para Simplificar o home.php

--- paginas/function.php
+++ paginas/function.php
@@ -6,5 +6,30 @@
         return null;
     }
 ?>
-
+<?php 
+    function printTodosProdutos($array){
+        $cont = 1;
+        for($i =1;$i<=17;){
+            echo '
+            <div id="contact">
+            <address>
+                <h1>'.$array[$i]['titulo'].'</h1>
+            </address>
+            </div>';
+            
+            for($j=1;$j<4;$j++){
+                echo '
+                <article class="gallery-card">
+                    <a href="paginas/desc_produto.php?id='.$cont.'" target="_blank">
+                    <img src="imagens/'.$array[$cont]['img-caminho'].'" width="250" height="190" alt="Torta de Morango">
+                    </a>
+                    <p class="gallery-title">'.$array[$cont]['nome'].'</p>
+                </article>';
+                    $cont ++;
+                }
+                $i +=3;
+        }
+        return null;   
+    }
+?>
  
