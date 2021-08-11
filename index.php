0a70287f09f69a6e043cba34db6b00468eb7ec96
Fernando Almeida <f_silvadealmeida@hotmail.com>
2021-08-11 12:52:30 -0300

    Corrigindo os problemas do cabeçalho

--- index.php
+++ index.php
@@ -1,24 +1,8 @@
-<!doctype html>
-<html lang="pt-br">
-    <head>
-        <title> Doceria </title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    </head>
-    <body>
-    <div id="principal">
-        <header class="header-top">
-            <nav class="header-nav">
-                <ul>
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="paginas/contato.php">Contato</a></li>
-                    <li><a href="paginas/quem-somos.php">Quem Somos?</a></li>
-                    <li><a href="paginas/localizacao.php">Localização</a></li>
-                </ul>
-            </nav>
-        </header>
-        <img src="imagens/fundo-topo2.jpg" width="1024px" height="400" alt="Modelo femino comendo doce">
+
+    <?php 
+        $index =1;
+        include 'layout/cabecalho.php';
+    ?>
 
         <article class="portfolio">
             <h1>Doce Verão - Doces da Dona Rose</h1>
--- layout/cabecalho.php
+++ layout/cabecalho.php
@@ -1,9 +1,20 @@
 <!doctype html>
 <html lang="pt-br">
+    <?php
+        if(!isset($index)){
+            $index = 0;
+        }
+    ?>
     <head>
         <title> Doceria </title>
         <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="../css/style.css">
+        <link rel="stylesheet" type="text/css" href="
+        <?=
+            $index == 1 ? "css/style.css" : "../css/style.css";
+        ?>
+        
+        
+        ">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     </head>
     <body>
@@ -12,12 +23,12 @@
             <header class="header-top">
                 <nav class="header-nav">
                     <ul>
-                        <li><a href="../index.php">Home</a></li>
-                        <li><a href="contato.php">Contato</a></li>
-                        <li><a href="quem-somos.php">Quem Somos?</a></li>
-                        <li><a href="localizacao.php">Localização</a></li>
+                        <li><a href="<?= $index == 1 ? "": "../"; ?>index.php">Home</a></li>
+                        <li><a href="<?= $index == 1 ? "paginas/":"" ?>contato.php">Contato</a></li>
+                        <li><a href="<?= $index == 1 ? "paginas/":"" ?>quem-somos.php">Quem Somos?</a></li>
+                        <li><a href="<?= $index == 1 ? "paginas/":"" ?>localizacao.php">Localização</a></li>
                     </ul>
                 </nav>
             </header>
-            <img src="../imagens/fundo-topo2.jpg" width="1024px" height="400" alt="Modelo femino comendo doce">
+            <img src="<?= $index == 1 ? "":"../" ?>imagens/fundo-topo2.jpg" width="1024px" height="400" alt="Modelo femino comendo doce">
  
